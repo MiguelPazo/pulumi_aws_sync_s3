38911df1f17c405f11d5bddceb3ab23e669ea13e
Miguel Pazo <miguel.ps19@gmail.com>
2021-04-03 17:58:56 -0500

    first commit

---
+++ index.ts
@@ -0,0 +1,53 @@
+/**
+ * Created by Miguel Pazo (https://miguelpazo.com)
+ */
+
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as fs from "fs";
+import * as mime from "mime";
+import * as path from "path";
+import * as config from "./config";
+
+const mainBucket = aws.s3.getBucket({
+    bucket: config.bucketName,
+});
+
+const webContentPath = path.join(process.cwd(), 'data');
+console.log("Syncing contents from local disk at", webContentPath);
+
+crawlDirectory(
+    webContentPath,
+    (filePath: string) => {
+        const relativeFilePath = filePath.replace(webContentPath + "/", "");
+
+        console.log(relativeFilePath);
+
+        new aws.s3.BucketObject(
+            relativeFilePath,
+            {
+                key: relativeFilePath,
+                acl: "public-read",
+                bucket: mainBucket.then(bucket => bucket.bucket),
+                contentType: mime.getType(filePath) || undefined,
+                source: new pulumi.asset.FileAsset(filePath),
+            });
+    }
+);
+
+function crawlDirectory(dir: string, f: (_: string) => void) {
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+        const filePath = `${dir}/${file}`;
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            crawlDirectory(filePath, f);
+        }
+
+        if (stat.isFile()) {
+            f(filePath);
+        }
+    }
+}
